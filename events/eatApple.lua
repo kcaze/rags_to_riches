458c6b7deb69002d5443f3e637a844a356b022c3
Pierre Karashchuk <krchtchk@gmail.com>
2019-10-04 21:05:35 -0700

    eat apple event

---
+++ events/eatApple.lua
@@ -0,0 +1,31 @@
+require('utils')
+local apple = require("../items/apple")
+
+return {
+  name = "You consider eating an Apple",
+  condition = function (state)
+     table.contains(state.items, apple)
+  end,
+  description = "There is a tasty apple in your pocket. You admire it carefully, considering whether or not to eat it.",
+  heads = {
+    description = "You wolf down the apple. It fills you up with that nice apple aroma.",
+    effectDescription = "-1 Apple, +5 HP",
+    effect = function (state)
+      table.delete(state.items, apple)
+      state.hp = state.hp + 5;
+    end
+  },
+  tails = {
+    description = 'You flaunt the apple. How beautiful! How dazzling! How tasty! A stranger is convinced and gives you 5 coins for it.',
+    effectDescription = "-1 Apple, +5 coins",
+    effect = function (state)
+      state.coins = state.coins + 5
+    end
+  },
+  beg = {
+    description = 'You look at the apple carefully. "Not this time, you little fruit!" you mutter as you put it away.',
+    effectDescription = "Nothing",
+    effect = function (state)
+    end
+  }
+}
