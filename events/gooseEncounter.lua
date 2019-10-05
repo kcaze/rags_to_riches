b294f8f2888bc5031a9e695e90fc957044e18f9e
Herman Chau <beijingherman@gmail.com>
2019-10-05 15:56:27 -0700

    Add goose event.

---
+++ events/gooseEncounter.lua
@@ -0,0 +1,49 @@
+require('utils')
+local image = require("../image")
+
+return {
+  name = "You encounter an untitled goose.",
+  amount = 5,
+  weight = 0.2,
+  condition = function (state)
+    return state.coins >= 5
+  end,
+  description = "The mischievous goose honks at you.",
+  heads = {
+    effectDescription = "+25 coin (30%)",
+    effect = function (state)
+      if love.math.random() <= 0.3 then
+        state.coins = state.coins + 25 
+        return {
+          description = "Holy crap, the goose laid a gold egg! You take it and sell it for $25.",
+          image = image.gooseWithGoldEgg
+        }
+      else 
+        return {
+          description = "HOOOONNNNNKKK!",
+          image = image.goose
+        }
+      end
+    end
+  },
+  tails = {
+    effectDescription = "+8 coin",
+    effect = function (state)
+      state.coins = state.coins + 8 
+      return {
+        description = "You steal the goose's egg and sell it for 8 coin.",
+        image = image.gooseWithBlueEgg
+      }
+    end
+  },
+  beg = {
+    effectDescription = "+1 coin",
+    effect = function (state)
+      state.coins = state.coins + 1 
+      return {
+        description = "Is that a... stone egg? You steal it anyways and manage to sell it off for 1 coin.",
+        image = image.gooseWithStoneEgg
+      }
+    end
+  }
+}
