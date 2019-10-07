eccdf0846bf73534fa8d2e615f1a7c48e35e0680
Pierre Karashchuk <krchtchk@gmail.com>
2019-10-06 17:41:39 -0700

    add Ena's dealer and factory images

--- items/dealer.lua
+++ items/dealer.lua
@@ -3,5 +3,5 @@ local image = require('image')
 return {
   name = "Rat dealer",
   description = "This rat dealer could sell your rats and make you rich",
-  image = image.placeholder
+  image = image.dealer
 }
--- items/factory.lua
+++ items/factory.lua
@@ -3,5 +3,5 @@ local image = require('image')
 return {
   name = "Rat factory",
   description = "It's ... a rat factory?! Is demand for rats that high?",
-  image = image.placeholder
+  image = image.factory
 }
