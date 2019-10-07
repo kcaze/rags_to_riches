11fede8bc5b96cb21d5c4b0a111091f5d8ceb371
Pierre Karashchuk <krchtchk@gmail.com>
2019-10-07 11:48:31 -0700

    update values for negative events

--- events/dealerBringsMoney.lua
+++ events/dealerBringsMoney.lua
@@ -9,16 +9,16 @@ return {
   end,
   description = "One of your dealers has executed some trades!",
   heads = {
-    effectDescription = "-1 dealer, +50 coins (50%), -1 dealer (50%)",
+    effectDescription = "-1 dealer, +100 coins (50%), -1 dealer (50%)",
     effect = function (state)
       local p = love.math.random()
       table.delete(state.items, item.dealer)
       state.ratsSold = state.ratsSold + 4
       if p < 0.5 then
-        state.coins = state.coins + 50
+        state.coins = state.coins + 100
         return {
-          description = [[You negotiate a really good deal and obtain 50 coins!]],
-          image = image.plus50,
+          description = [[You negotiate a really good deal and obtain 100 coins!]],
+          image = image.plus100,
         }
       else
         return {
@@ -29,34 +29,34 @@ return {
     end
   },
   tails = {
-    effectDescription = "-1 dealer, +20 coins",
+    effectDescription = "-1 dealer, +50 coins",
     effect = function (state)
-      state.coins = state.coins + 20
+      state.coins = state.coins + 50
       table.delete(state.items, item.dealer)
       state.ratsSold = state.ratsSold + 4
       return {
-        description = [[You collect 20 coins as per usual.]],
-        image = image.plus20,
+        description = [[You collect 50 coins as per usual.]],
+        image = image.plus50,
       }
     end
   },
   beg = {
-    effectDescription = "-1 dealer +15 coins (50%), +5 coins (50%)",
+    effectDescription = "-1 dealer +25 coins (50%), +10 coins (50%)",
     effect = function (state)
       local p = love.math.random()
       if p < 0.5 then
         table.delete(state.items, item.dealer)
         state.ratsSold = state.ratsSold + 4
-        state.coins = state.coins + 15
+        state.coins = state.coins + 25
         return {
-          description = [[You negotiate with the dealer, and he hands you 15 coins.]],
-          image = image.plus15,
+          description = [[You negotiate with the dealer, and he hands you 25 coins.]],
+          image = image.plus25,
         }
       else
-        state.coins = state.coins + 5
+        state.coins = state.coins + 10
         return {
-          description = [[The dealer pities you and hands you 5 coins.]],
-          image = image.plus5,
+          description = [[The dealer pities you and hands you 10 coins.]],
+          image = image.plus10,
         }
         
       end
